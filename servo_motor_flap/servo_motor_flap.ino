8b17a1f189f943fb611700f3b5e36bb87f39e3fe
Amazing Cabiles <amazinggracecabiles@gmail.com>
2023-04-24 15:16:41 +0800

    added weight code

--- servo_motor_flap/servo_motor_flap.ino
+++ servo_motor_flap/servo_motor_flap.ino
@@ -1,22 +1,21 @@
 #include<Servo.h>
-// LCD
+// LCD - attach sda to pin 4 & scl to pin 5
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-// paper shredder flap
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-Servo myservo;
 
-int pos = 0;
+// Servo myservo;
+// int pos = 0;
 
 //pwede pa tong mabago kasi depending sa weight
-char lcdGrams = 0;
+int lcdGrams = 0;
 
 void setup() {
   // lcd start
   lcd.init();
   lcd.backlight();
   // servo pin 7
-  myservo.attach(7);
+  // myservo.attach(7);
 }
 
 void loop() {
@@ -26,20 +25,21 @@ void loop() {
   lcd.print("Target: 12 grams");
   lcd.setCursor(0,1);
   // dapat magbabago to sa lcd depending sa weight 
+  lcd.print("Weight: ");
   lcd.print(lcdGrams);
+  lcd.print(" grams");
   delay(2000);
   lcd.clear();
 
   //shredder
-
   //weight
 
   // open the flap
-for (pos=0; pos<=180; pos+=1){
-  myservo.write(pos);
-  delay(15);
-}
-  for (pos=180; pos>=0; pos-=1)
-  myservo.write(pos);
-  delay(15);
+// for (pos=0; pos<=180; pos+=1){
+//   myservo.write(pos);
+//   delay(15);
+// }
+//   for (pos=180; pos>=0; pos-=1)
+//   myservo.write(pos);
+//   delay(15);
 }
---
+++ weight_scale_code/Weight_Scale.ino
@@ -0,0 +1,27 @@
+#include <HX711_ADC.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+HX711_ADC LoadCell(4, 5);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup(){
+  // weight & turn on lcd
+  LoadCell.begin();
+  LoadCell.start(2000);
+  LoadCell.setCalFactor(1000.0);
+  lcd.init();
+  lcd.backlight();
+}
+
+void loop(){
+  LoadCell.update();
+  float i = LoadCell.getData();
+  lcd.setCursor(0, 0);
+  lcd.print("Weight [g]:");
+  lcd.setCursor(0, 1);
+  lcd.print(i);
+}
+
+// gawa ng function na mag mag bubukas ng servo flap after 12 grams pati 
+// limit ay yung 12 grams - nagana ang code wag idelete
